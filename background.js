a607ab66861c14a628ddc27ccf9f14467c2bc23a
Vera Wesselkamp <vera.wesselkamp@inria.fr>
2020-09-18 15:06:19 +0200

    Add extraction of request and response cookies

--- background.js
+++ background.js
@@ -15,9 +15,6 @@ class HttpInfo{
     this.tabId = webRequest.tabId;
     this.domain = getSecLevelDomain(webRequest.url);
     this.party = this.checkIfThirdParty(); // move this function into the object?
-    this.header = webRequest.requestHeaders;
-    // this.cookies = extractCookie(this.header);
-    // console.log(this.cookies)
   }
 
   checkIfThirdParty(){
@@ -29,17 +26,85 @@ class HttpInfo{
 }
 
 class RequestInfo extends HttpInfo{
+  constructor(webRequest) {
+    super(webRequest);
+    this.header = webRequest.requestHeaders;
+    this.cookies = this.extractCookie(this.header);
+  }
+
   archive(tabId){
     tabs[tabId].requests.push(this);
     if (tabId === currentTab) {
-      notifyPopupOfNewRequests(this);
+      this.notifyPopupOfNewRequests(this);
     }
+  }
+
+  notifyPopupOfNewRequests(request) {
+    //Could not establish connection. Receiving end does not exist.
+    //differntiate between if popup open or not? or simply handleError
+    var sending = browser.runtime.sendMessage({
+      request: request
+    });
+
+    function handleError() {
+      return;
+    }
+
+    function handleResponse() {
+      return;
+    }
+
+    sending.then(handleResponse, handleError);
+  }
+
+  extractCookie(header) {
+    header.forEach((attribute) => {
+      if (attribute.name.toLowerCase() === "cookie") {
+        return this.extractCookieFromHeader(this.url, attribute.value);
+      }
+    })
+    return [];
+  }
+
+  extractCookieFromHeader(url, headerCookies){
+    let cookies = [];
+    // console.log(headerCookies);
+    let result = headerCookies
+        .split(';')
+        .map(v => v.split(/=(.+)/)); // returns emptz string as third parameter for some reason
+    result.forEach((cookie) => {
+      cookies.push(new Cookie(url, cookie[0], cookie[1]));
+    });
+    return cookies;
   }
 }
 
 class ResponseInfo extends HttpInfo{
+  constructor(webRequest) {
+    super(webRequest);
+    this.header = webRequest.responseHeaders;
+    this.cookies = this.extractCookie(this.header);
+  }
+
   archive(tabId){
     tabs[tabId].responses.push(this);
+  }
+
+  extractCookie(header) {
+    let cookies = [];
+    header.forEach((attribute) => {
+      if (attribute.name.toLowerCase() === "set-cookie") {
+        cookies.push(this.extractCookieFromHeader(this.url, attribute.value));
+      }
+    })
+    return cookies;
+  }
+
+  extractCookieFromHeader(url, headerCookies){
+    let result = headerCookies
+        .split(';', 1)
+        .map(v => v.split(/=(.+)/)); // returns emptz string as third parameter for some reason
+    return new Cookie(url, result[0][0], result[0][1]); //??
   }
 }
 
@@ -49,56 +114,37 @@ function Cookie(url, key, value){
   this.value = value;
 }
 
-function extractCookie(header) {
-  header.forEach((attribute) => {
-    // console.log(attribute.name);
-    if (attribute.name == "Set-Cookie" || attribute.name == "Cookie") {
-      //return extractCookieFromHeader(this.url, attribute.value);
-      // console.log(attribute.value);
-      return attribute.value;
-    }
-  })
-  return null;
-}
-
-function extractCookieFromHeader(url, headerCookies){
-  let cookies = [];
-  // console.log(headerCookies);
-  headerCookies.forEach((cookie) => {
-    cookies.push(new Cookie(url, cookie.name, cookie.value))
-  })
-  return cookies;
-}
-
-function cleanTab(tabId) {
+function initializeCleanTab(tabId) {
   browser.tabs.get(tabId).then((tab) => {
     tabs[tabId] = new TabInfo(tab);
   })
 }
 
-function logHeader(requestDetails, webRequest) {
+function logHeader(requestDetails) {
   if(tabs[requestDetails.tabId] == undefined){
-    cleanTab(requestDetails.tabId);
+    console.log("tab unclear")
+    initializeCleanTab(requestDetails.tabId);
   } else if (requestDetails.originUrl == undefined){
     console.log("undefined request info " + requestDetails.url)
   }
-
-  webRequest.archive(requestDetails.tabId);
 }
+
 function logRequest(requestDetails) {
+  logHeader(requestDetails);
   let request = new RequestInfo(requestDetails);
-  logHeader(requestDetails, request);
+  request.archive(requestDetails.tabId);
 }
 
 function logResponse(responseDetails) {
+  logHeader(responseDetails);
   let response = new ResponseInfo(responseDetails);
-  logHeader(responseDetails, response);
+  response.archive(responseDetails.tabId);
 }
 
-function clearTab(tabId, changeInfo, tabInfo){
+function clearTabsData(tabId, changeInfo, tabInfo){
   setCurrentTab();
   if(changeInfo.url && changeInfo.status == "loading") {
-    cleanTab(tabId)
+    initializeCleanTab(tabId)
     console.log("clearing " + tabInfo.url)
   }
 }
@@ -119,34 +165,12 @@ function getActiveTab() {
   return browser.tabs.query({active: true, currentWindow: true});
 }
 
-function notifyPopupOfNewRequests(request) {
-  //Could not establish connection. Receiving end does not exist.
-  //differntiate between if popup open or not? or simply handleError
-  var sending = browser.runtime.sendMessage({
-    request: request
-  });
-
-  function handleError() {
-    return;
-  }
-
-  function handleResponse() {
-    return;
-  }
-
-  sending.then(handleResponse, handleError);
-}
-
-const clearFilter = {
-  properties: ["status"]
-}
-
 
 setCurrentTab();
 
 function initializeAllTabs(tabs) {
   for (let tab of tabs) {
-    cleanTab(tab.id);
+    initializeCleanTab(tab.id);
   }
 }
 
@@ -158,7 +182,10 @@ let querying = browser.tabs.query({});
 querying.then(initializeAllTabs, onError);
 
 // update when the tab is updated
-browser.tabs.onUpdated.addListener(clearTab, clearFilter);
+const clearFilter = {
+  properties: ["status"]
+}
+browser.tabs.onUpdated.addListener(clearTabsData, clearFilter);
 // update when the tab is activated
 browser.tabs.onActivated.addListener(setCurrentTab);
 
