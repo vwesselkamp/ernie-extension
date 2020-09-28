51f69b4255b17481ac4b6903bafb777fa436bf15
Vera Wesselkamp <vera.wesselkamp@inria.fr>
2020-09-28 12:10:26 +0200

    Display responses as well as requests

--- popup/show_urls.js
+++ popup/show_urls.js
@@ -1,7 +1,7 @@
 var thirdParty_mode = false;
 
 
-function insertUrl(request) {
+function insertRequest(request) {
   let node;
 
   function listIdentifyingCookies() {
@@ -34,6 +34,41 @@ function insertUrl(request) {
     hideElement(node);
   }
   document.getElementById("urls").appendChild(node);
+}
+
+function insertResponse(response) {
+  let node;
+
+  function listIdentifyingCookies() {
+    node = document.createElement("details");
+    let summary = document.createElement("summary");
+    summary.innerText = response.domain + " : " + response.url;
+    node.appendChild(summary)
+    for (let i in response.cookies) {
+      if (response.cookies[i].identifying) {
+        let cookie = document.createElement("div");
+        cookie.innerText = response.cookies[i].key;
+        node.appendChild(cookie);
+      }
+    }
+  }
+
+  function listOnlyUrl() {
+    node = document.createElement("div");
+    node.innerText = response.domain + " : " + response.url;
+  }
+
+  if (response.cookies.filter(cookie => cookie.identifying === true).length > 0) {
+    listIdentifyingCookies();
+  } else {
+    listOnlyUrl();
+  }
+
+  node.className = response.category + " " + response.party + " url";
+  if(response.party === "first") {
+    hideElement(node);
+  }
+  document.getElementById("response-urls").appendChild(node);
 }
 
 //either hides all non relevant items or displays them
@@ -78,7 +113,10 @@ function constructPage() {
   document.getElementById("current-page").innerHTML = "Page: " + backgroundPage.tabs[backgroundPage.currentTab].domain;
   document.getElementById("urls").innerHTML = "";
   backgroundPage.tabs[backgroundPage.currentTab].requests.forEach((request, i) => { // error if Tab not initilized
-    insertUrl(request);
+    insertRequest(request);
+  });
+  backgroundPage.tabs[backgroundPage.currentTab].responses.forEach((response, i) => { // error if Tab not initilized
+    insertResponse(response);
   });
   setStats(backgroundPage.tabs[backgroundPage.currentTab]);
 }
@@ -91,21 +129,21 @@ getting.then(async (page) => {
 });
 
 
-document.getElementById("thirdParty_button").addEventListener("click", function(){
-  document.getElementById("popup-title").innerHTML = "3rd Party Requests";
-  thirdParty_mode = true;
-  toggleMode()
-});
-
-document.getElementById("all").addEventListener("click", function(){
-  document.getElementById("popup-title").innerHTML = "All Requests";
-  thirdParty_mode = false;
-  toggleMode();
-});
+// document.getElementById("thirdParty_button").addEventListener("click", function(){
+//   document.getElementById("popup-title").innerHTML = "3rd Party Requests";
+//   thirdParty_mode = true;
+//   toggleMode()
+// });
+//
+// document.getElementById("all").addEventListener("click", function(){
+//   document.getElementById("popup-title").innerHTML = "All Requests";
+//   thirdParty_mode = false;
+//   toggleMode();
+// });
 
 function evaluateMessage(message) {
   if (message.request) {
-    insertUrl(message.request);
+    insertRequest(message.request);
     setStats(backgroundPage.tabs[message.request.tabId]); // this is potentially very slow
   } else if (message.reload) {
     constructPage();
